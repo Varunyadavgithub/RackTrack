d8c5476b7e4406a2b7ebebd3c9539d6ec8c09617
Varunyadavgithub <yadav.varun056038@gmail.com>
2025-12-29 08:58:01 +0530

    Wrap the app with redux provider

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,10 +1,20 @@
 import { Stack } from "expo-router";
 import SafeScreen from "@/components/SafeScreen";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+
+const persistor = persistStore(store);
 
 export default function RootLayout() {
   return (
-    <SafeScreen>
-      <Stack screenOptions={{ headerShown: false }} />
-    </SafeScreen>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeScreen>
+          <Stack screenOptions={{ headerShown: false }} />
+        </SafeScreen>
+      </PersistGate>
+    </Provider>
   );
 }
